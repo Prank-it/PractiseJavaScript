a92a9d0ab7adc9634fef5eeed3bbe9dd79819753
Prankit Srivastavaa <98051159+Prank-it@users.noreply.github.com>
2026-01-28 10:50:02 +0000

    basics1Completed

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,1 +1,8 @@
- 
+ const names = "prankit";
+ const repocount = 50;
+ console.log(`my name is ${names}`);
+
+ //another way of declaring a string
+ const gameName = new String("prankit");
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);//there are certain funtion like length , charAt, toUppercase in prototype  
